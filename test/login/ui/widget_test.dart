64db1aed0f46a4c66614a388309bdcbb149ec8f4
Pay Nep Pvt Ltd <payneppvtltd@Pays-MacBook-Pro.local>
2021-04-15 19:12:27 +0545

    login validation

--- test/login/ui/widget_test.dart
+++ test/login/ui/widget_test.dart
@@ -9,12 +9,12 @@ import 'package:wallet/features/login/ui/login_screen.dart';
 void main() {
   testWidgets('Login  Widget Test', (tester) async {
     final testWidget = MaterialApp(
-        home: BlocProvider<LoginBloc>(
-            create: (_) => LoginBloc(), child: LoginFeatureWidget()));
+        home:  LoginFeatureWidget());
 
     await tester.pumpWidget(testWidget);
     await tester.pump(Duration(milliseconds: 1000));
     expect(find.byType(LoginPresenter), findsOneWidget);
+    expect(find.byType(LoginFeatureWidget), findsOneWidget);
     expect(find.byType(LoginScreen), findsOneWidget);
     expect(find.byKey(ValueKey("password")),findsOneWidget);
     expect(find.byKey(ValueKey("user_id")),findsOneWidget);
